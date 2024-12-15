a80725450dad325e2f08de09647f0aa77f398a91
Shuva Kharel <shuvakharel007@gmail.com>
2024-12-16 02:02:34 +0545

    Backend Ready

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is running on port: ", PORT);
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-gray-900 via-green-900'>
+
+    </div>
+  )
+}
+
+export default App;
